c99e3031143a01107ec7ac7d1797e84754de4362
Meenachi Sundaresan J <139699517+jms-luck@users.noreply.github.com>
2026-02-08 19:16:55 +0530

    api removed

--- frontend/editor/src/firebase.js
+++ frontend/editor/src/firebase.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCh_FuaaqVgX01smPNV_Lj_zn5kzwZXCMg",
-  authDomain: "ditor-e2486.firebaseapp.com",
-  projectId: "ditor-e2486",
-  storageBucket: "ditor-e2486.firebasestorage.app",
-  messagingSenderId: "129270848040",
-  appId: "1:129270848040:web:1e416bd13029f0626fb31d",
-  measurementId: "G-24GYPWFSY6"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
